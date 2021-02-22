5878cb5cd54ab47c49b209fcb942c11801da3501
Raviteja Guttula <hello@thisisgvrt.me>
2021-02-22 14:08:41 -0800

    Fixed missing import

--- pages/Home.js
+++ pages/Home.js
@@ -1,4 +1,4 @@
-import React, { Component,useRef } from 'react';
+import React, { Component, useRef } from 'react';
 import { connect } from 'react-redux';
 import { createStore } from 'redux';
 import { Actions } from 'react-native-router-flux';
@@ -22,22 +22,22 @@ import {
   Title,
   View,
 } from 'native-base';
-import  { DocumentPickerResponse } from 'react-native-document-picker';
+import DocumentPicker, { DocumentPickerResponse } from 'react-native-document-picker';
 
 
 const Home = ({
-  
+
   isLoggedIn,
   dispatch,
 }) => {
   const [books, setBooks] = React.useState([]);
-  React. useEffect(() => {
+  React.useEffect(() => {
     // clickForSearch("");
-  },[]);
-  const book=null;
+  }, []);
+  const book = null;
   const webview = useRef();
 
-  bookChooser= (e)=> { 
+  bookChooser = (e) => {
     // console.log("hello")
     // var firstFile = e;
     // alert(firstFile)
@@ -57,48 +57,48 @@ const Home = ({
     // } else {
     //     alert("Your browser does not support the required features. Please use a modern browser such as Google Chrome, or Mozilla Firefox");
     // }
-};
+  };
 
 
 
-prev=()=>{
+  prev = () => {
     book.prevPage();
-}
+  }
 
-next=()=>{
+  next = () => {
     book.nextPage();
-}
+  }
 
 
 
-handleFilePicker= async () => {
-   // console.log(bookSearch)
-    
-   try {
-    const res = await DocumentPicker.pick({
-      type: [DocumentPicker.types.images],
-    });
-    console.log(
-      res.uri,
-      res.type, // mime type
-      res.name,
-      res.size
-    );
-  } catch (err) {
-    if (DocumentPicker.isCancel(err)) {
-      // User cancelled the picker, exit any dialogs or menus and move on
-    } else {
-      throw err;
+  handleFilePicker = async () => {
+    // console.log(bookSearch)
+
+    try {
+      const res = await DocumentPicker.pick({
+        type: [DocumentPicker.types.images],
+      });
+      console.log(
+        res.uri,
+        res.type, // mime type
+        res.name,
+        res.size
+      );
+    } catch (err) {
+      if (DocumentPicker.isCancel(err)) {
+        // User cancelled the picker, exit any dialogs or menus and move on
+      } else {
+        throw err;
+      }
     }
+
+
   }
-   
-   
-  }
-  
-  
-  
-  
-  
+
+
+
+
+
   _pickDocument = async () => {
     // let result = await DocumentPicker.getDocumentAsync({});
     // // need to verify whether doc is epub
@@ -109,7 +109,7 @@ handleFilePicker= async () => {
     // var linkname = result.uri;
     // var bname = toTitleCase(onlyname);
     // db.transaction(tx => {
-      
+
     //   tx.executeSql(
     //     'CREATE TABLE IF NOT EXISTS BooksTable (book_id INTEGER PRIMARY KEY AUTOINCREMENT,name, link, flag, author,imgpath)'
     //     )    
@@ -117,53 +117,53 @@ handleFilePicker= async () => {
     //       linkname, 0, '', '']);
     //      // console.log(result);
     //     })
-        
-        
-        
+
+
+
     //   }
     //   function toTitleCase(str) {
     //     return str.replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); });
     //   }
-      
-      // const ebook =() => {
-      //   return (
-      //     <Epub src={"https://s3.amazonaws.com/epubjs/books/moby-dick/OPS/package.opf"}
-      //     flow={"paginated"} />
-      
-      //   )}
-      
+
+    // const ebook =() => {
+    //   return (
+    //     <Epub src={"https://s3.amazonaws.com/epubjs/books/moby-dick/OPS/package.opf"}
+    //     flow={"paginated"} />
+
+    //   )}
+
   }
-      return (
-        <Container>
-        <Header style={{ backgroundColor: 'black' }}>
-          
-          <Left>
-  
+  return (
+    <Container>
+      <Header style={{ backgroundColor: 'black' }}>
+
+        <Left>
+
           <Icon.Button name='cog' dark backgroundColor="black">
             <Text style={{ color: "white" }}>Settings</Text>
           </Icon.Button>
-          </Left>
-          {/* <Body>
+        </Left>
+        {/* <Body>
           <Text  style={{ color: "white"}}> HOME </Text>  
           </Body> */}
-          <Right>
+        <Right>
           <Icon.Button name='plus' onPress={handleFilePicker} dark backgroundColor="black">
             <Text style={{ color: "white" }}>Add Books</Text>
           </Icon.Button>
-          </Right>
-        </Header>
-        
-        <Content >
-      
-              <FormItem rounded>
-              <Input placeholder='Search'/>
-              <Button onclick= "">
+        </Right>
+      </Header>
+
+      <Content >
+
+        <FormItem rounded>
+          <Input placeholder='Search' />
+          <Button onclick="">
             <Text>
               GO
             </Text>
           </Button>
-            </FormItem>
-            </Content>
+        </FormItem>
+      </Content>
 
       <Footer >
         <FooterTab style={{ backgroundColor: 'black' }} >
@@ -178,7 +178,7 @@ handleFilePicker= async () => {
           <Button vertical >
             <Text>Statistics</Text>
           </Button>
-          <Button vertical  onPress={() => {
+          <Button vertical onPress={() => {
             Actions.learning();
           }}>
             <Text>Learning</Text>
@@ -186,17 +186,16 @@ handleFilePicker= async () => {
         </FooterTab>
       </Footer>
     </Container>
-        
-       
-          )
-        }
+
+
+  )
+}
 
 
 
-        const mapStateToProps = (state) => {
-          return {
-            isLoggedIn: state.userReducer.isLoggedIn,
-          };
-        };
- export default connect(mapStateToProps)(Home);
-        
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.userReducer.isLoggedIn,
+  };
+};
+export default connect(mapStateToProps)(Home);
